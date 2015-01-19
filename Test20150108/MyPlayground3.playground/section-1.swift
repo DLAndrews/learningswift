2c759d4fc5a5975ac52005b14987848cc9237dcf
DLAndrews <dleoandrews@outlook.com>
2015-01-19 23:05:26 +0900

    20150119练习

--- Test20150108/MyPlayground3.playground/section-1.swift
+++ Test20150108/MyPlayground3.playground/section-1.swift
@@ -27,3 +27,61 @@ var b1=2
 var c=1
 var a2=a1 ?? (b1==(++c))
 c
+
+
+var shoppingList = ["Eggs", "Milk"]
+shoppingList.append("Flour")
+// shoppingList 现在有3个数据项，有人在摊煎饼
+shoppingList += ["Baking Powder"]
+// shoppingList 现在有四项了
+shoppingList += ["Chocolate Spread","Cheese","Butter"]
+// shoppingList 现在有七项了
+
+shoppingList[6]
+
+shoppingList[4...6] = ["Bananas", "Apples"]
+// shoppingList 现在有六项
+
+shoppingList[5]
+
+let der:[Int]=[1,2,3,4]
+
+var ass=2
+var vcc = ++ass
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    println("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    println("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    println("(\(x), \(y)) is just some arbitrary point")
+}
+// 输出 "(1, -1) is on the line x == -y"
+
+let a=4
+
+switch a{
+case 1...5:
+    println("I")
+    fallthrough
+case 2,3:println("AM")
+case 3,4,5:println("Peng")
+default:println("!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
